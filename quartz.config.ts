d10d42d71ba03d13f36b5779067c4f1a2ab6148e
Zafer Altınsoy <81522783+zaltinsoy@users.noreply.github.com>
2026-07-20 18:44:57 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "kishara-chronicles.github.io/interregnum-base",
+    baseUrl: "kishara-chronicles.github.io/interregnum",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
